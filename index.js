6990f1e2f25854402fadce57ae86cc76e0f3093d
Shrey <shrma.shray@gmail.com>
2024-01-24 22:10:31 +0530

    testing deployment

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const Stock = require('./data_models/stock_data')
 const User = require('./data_models/user');
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET;
-const indexPage = "./index.html";
-const registeredUserPage = "./forRegisteredUser.html";
-const loggedUserPage = "./forLoggedUser.html";
+const indexPage = process.cwd()+"/index.html";
+const registeredUserPage = process.cwd()+"/forRegisteredUser.html";
+const loggedUserPage = process.cwd()+"/forLoggedUser.html";
 var index;
 var registered;
 var logged;
